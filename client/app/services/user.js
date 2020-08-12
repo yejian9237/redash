e78a0ff11a7acd741666b7e2285396ff46239895
handeer <451583132@qq.com>
2020-08-13 07:28:40 +0800

    zh-cn

--- client/app/services/user.js
+++ client/app/services/user.js
@@ -18,13 +18,13 @@ function enableUser(user) {
   return axios
     .delete(disableResource(user))
     .then(data => {
-      notification.success(`User ${userName} is now enabled.`);
+      notification.success(`用户 ${userName} 已启用！`);
       user.is_disabled = false;
       user.profile_image_url = data.profile_image_url;
       return data;
     })
     .catch(error => {
-      notification.error("Cannot enable user", getErrorMessage(error));
+      notification.error("不能启用用户！", getErrorMessage(error));
     });
 }
 
@@ -33,13 +33,13 @@ function disableUser(user) {
   return axios
     .post(disableResource(user))
     .then(data => {
-      notification.warning(`User ${userName} is now disabled.`);
+      notification.warning(`用户 ${userName} 已停用。`);
       user.is_disabled = true;
       user.profile_image_url = data.profile_image_url;
       return data;
     })
     .catch(error => {
-      notification.error("Cannot disable user", getErrorMessage(error));
+      notification.error("不能停用用户！", getErrorMessage(error));
     });
 }
 
@@ -48,11 +48,24 @@ function deleteUser(user) {
   return axios
     .delete(`api/users/${user.id}`)
     .then(data => {
-      notification.warning(`User ${userName} has been deleted.`);
+      notification.warning(`用户 ${userName} 已被删除！`);
       return data;
     })
     .catch(error => {
-      notification.error("Cannot delete user", getErrorMessage(error));
+      notification.error("不能删除用户！", getErrorMessage(error));
+    });
+}
+
+function activeUser(user) {
+  const userName = sanitize(user.name);
+  return axios
+    .post(`api/users/${user.id}/active`)
+    .then(data => {
+      notification.warning(`用户 ${userName} 已激活！默认密码为“dazdada”，请为其分配角色即可正常使用。`);
+      return data;
+    })
+    .catch(error => {
+      notification.error("不能激活用户！", getErrorMessage(error));
     });
 }
 
@@ -73,11 +86,22 @@ function regenerateApiKey(user) {
   return axios
     .post(`api/users/${user.id}/regenerate_api_key`)
     .then(data => {
-      notification.success("The API Key has been updated.");
+      notification.success("API Key 已经重新生成！");
       return data.api_key;
     })
     .catch(error => {
-      notification.error("Failed regenerating API Key", getErrorMessage(error));
+      notification.error("重新生成API Key失败！", getErrorMessage(error));
+    });
+}
+
+function resetPassword(user) {
+  return axios
+    .get(`api/users/${user.id}/reset_password`)
+    .then(data => {
+      notification.success("重置密码已成功。");
+    })
+    .catch(error => {
+      notification.error("重置密码不成功。", getErrorMessage(error));
     });
 }
 
@@ -86,13 +110,13 @@ function sendPasswordReset(user) {
     .post(`api/users/${user.id}/reset_password`)
     .then(data => {
       if (clientConfig.mailSettingsMissing) {
-        notification.warning("The mail server is not configured.");
+        notification.warning("电子邮件服务没有配置。");
         return data.reset_link;
       }
-      notification.success("Password reset email sent.");
+      notification.success("重置密码邮件已发送。");
     })
     .catch(error => {
-      notification.error("Failed to send password reset email", getErrorMessage(error));
+      notification.error("重置密码邮件发送不成功。", getErrorMessage(error));
     });
 }
 
@@ -101,13 +125,13 @@ function resendInvitation(user) {
     .post(`api/users/${user.id}/invite`)
     .then(data => {
       if (clientConfig.mailSettingsMissing) {
-        notification.warning("The mail server is not configured.");
+        notification.warning("电子邮件服务器没有配置。");
         return data.invite_link;
       }
-      notification.success("Invitation sent.");
+      notification.success("用户邀请邮件已发送。");
     })
     .catch(error => {
-      notification.error("Failed to resend invitation", getErrorMessage(error));
+      notification.error("用户邀请邮件发送失败。", getErrorMessage(error));
     });
 }
 
@@ -119,9 +143,11 @@ const User = {
   enableUser,
   disableUser,
   deleteUser,
+  activeUser,
   convertUserInfo,
   regenerateApiKey,
   sendPasswordReset,
+  resetPassword,
   resendInvitation,
 };
 
