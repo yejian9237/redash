7e9b63a9f481ac0f6b96f04ee0dfb4e1db7a3dff
handeer <451583132@qq.com>
2020-09-27 16:53:45 +0800

    zh-cn

--- viz-lib/src/visualizations/chart/Editor/ChartTypeSelect.jsx
+++ viz-lib/src/visualizations/chart/Editor/ChartTypeSelect.jsx
@@ -9,7 +9,7 @@ export default function ChartTypeSelect(props) {
       { type: "line", name: "线形图(Line)", icon: "line-chart" },
       { type: "column", name: "条形图(Bar)", icon: "bar-chart" },
       { type: "area", name: "面积图(Area)", icon: "area-chart" },
-      { type: "pie", name: "饼行图(Pie)", icon: "pie-chart" },
+      { type: "pie", name: "饼形图(Pie)", icon: "pie-chart" },
       { type: "scatter", name: "散点图(Scatter)", icon: "circle-o" },
       { type: "bubble", name: "气泡图(Bubble)", icon: "circle-o" },
       { type: "heatmap", name: "热力图(Heatmap)", icon: "th" },
