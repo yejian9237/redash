fb9f55159d9917964fbeb7715906375375367b9c
handeer <451583132@qq.com>
2020-10-20 07:29:27 +0800

    zh-cn

--- client/app/components/ApplicationArea/index.jsx
+++ client/app/components/ApplicationArea/index.jsx
@@ -3,6 +3,10 @@ import routes from "@/services/routes";
 import Router from "./Router";
 import handleNavigationIntent from "./handleNavigationIntent";
 import ErrorMessage from "./ErrorMessage";
+import ConfigProvider from 'antd/lib/config-provider';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 
 export default function ApplicationArea() {
   const [currentRoute, setCurrentRoute] = useState(null);
@@ -33,5 +37,11 @@ export default function ApplicationArea() {
     return <ErrorMessage error={unhandledError} />;
   }
 
-  return <Router routes={routes.items} onRouteChange={setCurrentRoute} />;
+  moment.locale('zh-cn')
+
+  return (
+    <ConfigProvider locale={zh_CN}>
+      <Router routes={routes.items} onRouteChange={setCurrentRoute} />
+    </ConfigProvider>
+  );
 }
